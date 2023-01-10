699ca4b0307cb720d0513ceaf677cf2793bf45ab
RayPas <70732899+RayPas@users.noreply.github.com>
2023-01-09 22:18:23 -0800

    putting a book mark on this one

--- art-museum/src/components/GalleryView/index.js
+++ art-museum/src/components/GalleryView/index.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
+import ArtImageTile from "../ArtImageTile/index"
 
 const GalleryView = ({ galleries }) => {
+  // const art = galleries.map(images => images.url.toString())
   const { galleryId } = useParams();
   const title = galleries.find((id) => {
     const numberToMatch = id.id.toString();
@@ -10,10 +12,11 @@ const GalleryView = ({ galleries }) => {
     })
 
 
-console.log('')
+console.log(galleries)
   return (
   <div>
     {title && <h1>{title.name}</h1>}
+    {/* <ArtImageTile art={art} /> */}
   </div>
   )
 };
